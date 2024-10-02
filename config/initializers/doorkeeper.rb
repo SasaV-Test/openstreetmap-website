e16b390bec53c77369433801c6075d0987515283
SasaV-Test <131283636+SasaV-Test@users.noreply.github.com>
2024-10-02 22:13:12 +0200

    Update doorkeeper.rb

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -11,7 +11,7 @@ Doorkeeper.configure do
   resource_owner_authenticator do
     current_user
   end
-
+  api_version "v1"
   # If you didn't skip applications controller from Doorkeeper routes in your application routes.rb
   # file then you need to declare this block in order to restrict access to the web interface for
   # adding oauth authorized applications. In other case it will return 403 Forbidden response
